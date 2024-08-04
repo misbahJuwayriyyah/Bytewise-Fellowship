b29a581f2f2663aaf222ae6a1891564572cdd88a
Misbah Juwayriyyah <misbahjaveria32@gmail.com>
2024-08-04 21:05:17 +0500

    AI chatbot

---
+++ Week08/AI-chatbot/backend/src/index.ts
@@ -0,0 +1,6 @@
+//MERN Stack is a collection of Js technologies that work together to make a full stack web development environment.
+// -> A modern AI project which will use OPENAI API to create an ai real-time chatbot similar to ChatGPT. The application stores user's chats in a database to keep track of conversations, but a user can delete them anytime. The UI is capabale of showing of code blocks if requested by user.
+// Step 1: Setup Node, Express App with Typescript (github) -> npm install
+console.log(
+  "This is a starter kit for this amazing project. With 💓 By Indian Coders"
+);
